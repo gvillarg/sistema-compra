b25e66b42bc9e23632d89dd49c9d45c6d876183a
gvillarg@pucp.pe <gvillarg@pucp.pe@5526ecc2-414d-ccd6-bc3a-e31520ff4536>
2012-11-24 09:20:15 +0000

    filtrar Usuario completado

--- sistemaCompras/trunk/sistemaCompras/UsuarioDlg.cs
+++ sistemaCompras/trunk/sistemaCompras/UsuarioDlg.cs
@@ -13,52 +13,74 @@ namespace sistemaCompras
 {
     public partial class UsuarioDlg : Form
     {
-        private List<Usuario> lusuario;
-        private GestorUsuario gestorUsuario=GestorUsuario.Instancia();
+        private GestorUsuario gestorUsuario = GestorUsuario.Instancia();
+        List<Usuario> listaUsuario;
+        private GestorTipoUsuario gestorTipoUsuario = GestorTipoUsuario.Instancia();
+        List<TipoUsuario> listaTipoUsuario;
         private Usuario seleccionado = null;
         private int modo;
         public UsuarioDlg(int modo)
         {
+            listaTipoUsuario = gestorTipoUsuario.SeleccionarListaTipoUsuarios();
             InitializeComponent();
+            seleccionarUsuarios();
             actualizarTabla();
+            llenarCmbTipoUsuario();
+            
             this.modo = modo;
             if (modo == 0)
                 this.Text = "Modificar Usuario";
             else
                 this.Text = "Eliminar Usuario";
         }
+        public void seleccionarUsuarios()
+        {
+            listaUsuario = gestorUsuario.seleccionarUsuarios();
+        }
+        public void filtrarUsuarios()
+        {
+            String nombre = "%";
+            if (txtNombre.TextLength > 0)
+                nombre = "%" + txtNombre.Text + "%";
+
+            TipoUsuario tipo = null;
+            if (cmbTipo.SelectedIndex != 0)
+                tipo = gestorTipoUsuario.SeleccionarListaTipoUsuarios()[cmbTipo.SelectedIndex-1];
+            listaUsuario=gestorUsuario.filtrarUsuarios(nombre, tipo, cbMostrarEliminados.Checked);
+        }
         public void actualizarTabla()
         {
             while(tablaUsuario.Rows.Count>0)
                 tablaUsuario.Rows.RemoveAt(0);
-
-            lusuario = gestorUsuario.seleccionarUsuarios();
             String[] fila;
             Usuario u;
-            for (int i = 0; i < lusuario.Count; i++)
+            for (int i = 0; i < listaUsuario.Count; i++)
             {
-                u = lusuario[i];
+                u = listaUsuario[i];
                 fila = new String[] {""+u.getId(),u.getNombre(),u.getEmail(),
                     ""+u.getTelefono(),""+u.getSueldo(),u.getTipoUsuario().getDescripcion() };
                 tablaUsuario.Rows.Add(fila);
             }
         }
-
-
-
-
+        private void llenarCmbTipoUsuario()
+        {
+            cmbTipo.Items.Add("Seleccionar Todos");
+            for (int i = 0; i < listaTipoUsuario.Count; i++)
+                cmbTipo.Items.Add(listaTipoUsuario[i].getDescripcion());
+            cmbTipo.SelectedIndex=0;
+        }
         private void tablaUsuario_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (e.RowIndex >= 0 && e.RowIndex < lusuario.Count)
+            if (e.RowIndex >= 0 && e.RowIndex < listaUsuario.Count)
             {
-                seleccionado = lusuario[e.RowIndex];
+                seleccionado = listaUsuario[e.RowIndex];
             }
         }
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            gestorUsuario.filtrarUsuario(txtNombre.Text, lusuario[cmbTipo.SelectedIndex], cbMostrarEliminados.Checked);
-
+            filtrarUsuarios();
+            actualizarTabla();
         }
 
         private void botonAceptar_Click(object sender, EventArgs e)
